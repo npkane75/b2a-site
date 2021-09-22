55f1d3ac3977595c27173318d00df84b759feda5
npkane75 <neeraj.p.kane@gmail.com>
2021-09-22 14:48:46 -0700

    input is taking data

--- controller/postController.js
+++ controller/postController.js
@@ -4,25 +4,28 @@ const router = express.Router();
 
 
 
-router.get('/', (req, res) => {
-  res.send('hello world')
-})
+// router.get('/', (req, res) => {
+//   res.send('hello world')
+// })
 
 
 // Index Route - Retrieve many/all posts
 router.get('/', (req, res) => {
-  res.send("hello world");
-})
+  db.Post.find({}, (err, allPosts)=> {
+    if (err) return console.log(err);
 
-// New Route - Retrieve a form that can be used to create a new posts
+    res.render('index.ejs', {
+allPosts: allPosts
+    }); 
+  });
+});
+
+ 
+
+// New Route - send a form page that can be used to create a new posts
 router.get('/new', (req, res) => {
-  // db.Post.create(req.body, (err, createdPost)=>{
-  //   if (err) return console.log(err)
-  //   console.log(createdPost)
-    res.render('new.ejs')
-
-  // })
-})
+  res.render('new.ejs');
+});
 
 // Show Route - Retrieve one post  pass id?
 // router.get('/edit', (req, res) => {
@@ -32,23 +35,31 @@ router.get('/new', (req, res) => {
 
 // Create Route - Send data to create new post it adds to database and redirects
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('hello world')
-})
+  db.Post.create(req.body,(err, createdPost)=> {
+    // if (err) return console.log(err);
+    console.log(req.body)
+    res.render('index.ejs');
+  });
+});
 
 //Show info on one post
 router.get('/:id', (req, res) => {
-  db.Post.findbyId(req.params.id, (err, foundPost)=>{
-    if (err) return console.log(err)  
-  })
-  res.redirect('/', {onePost: foundPost}) 
-})
+  db.Post.findById(req.params.id)
+  .populate('posts')
+  .exec((err, foundPost)=>{
+    if (err) return console.log(err);
+    
+    console.log(foundPost); 
+
+    res.render('show.ejs',{post: postCreated });
+  });
+});
 
 
-//retrieve edit form for retrieved blog
-router.get('/:id/edit', (req, res) => { 
-  res.render('edit.ejs')
-})
+// //retrieve edit form for retrieved blog
+// router.get('/:id/edit', (req, res) => { 
+//   res.render('edit.ejs')
+// })
 
 //will update a particular blog and then redirect
 router.put('/:id', (req, res) => {
@@ -58,15 +69,19 @@ router.put('/:id', (req, res) => {
 console.log(createdPost);
 
 res.redirect('show.ejs');
-  })
-})
+  });
+});
 
 
 
 //delete a particular blog then redirect
 router.delete('/:id', (req, res) => {
-  res.send('hello world')
-})
+  db.Post.findByIdAndRemove(req.params.id, (err) => {
+		if (err) return console.log(err);
+
+		res.redirect('/new.ejs');
+	});
+});
 
 module.exports = router;
 
